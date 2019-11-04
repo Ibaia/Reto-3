cfb2871e67b7e09230acf77815d176ce1f0f25a7
Ibai acha <achita145@gmail.com>
2019-11-04 12:49:27 +0100

    Cambios

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
  
+<?php session_start();?>
 <!DOCTYPE html>
 
 
 <html lang="en">
 <head>
-<?php session_start();?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
