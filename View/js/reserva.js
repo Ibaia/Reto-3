8d1e7906fa09e1f766766a6d6acc99ab45f22c41
Ibai acha <achita145@gmail.com>
2019-11-05 09:21:35 +0100

    Cambios en admin y reservas

--- View/js/reserva.js
+++ View/js/reserva.js
@@ -73,8 +73,8 @@ $(document).ready(function() {
                     } else{
                         $('#vacio').remove();
                     }
-                    alert(strDate);
-                    alert($('#date').val());
+                    //alert(strDate);
+                    //alert($('#date').val());
                     /* compruba que la fecha escogida no sea anterior a la actual */
                     if($('#date').val()>strDate){
                         alert('Reservas no disponibles');
